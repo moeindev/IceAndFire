27bb6ea1b5b93556d62b8712640bf774988c1449
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2020-08-04 17:47:49 +0430

    calling quotes api for testing dagger

--- app/src/main/java/ir/moeindeveloper/iceandfire/di/module/IceAndFireModule.kt
+++ app/src/main/java/ir/moeindeveloper/iceandfire/di/module/IceAndFireModule.kt
@@ -43,39 +43,13 @@ class IceAndFireModule {
         val myCache = Cache(context.cacheDir, cacheSize)
 
         return OkHttpClient.Builder()
-            // Specify the cache we created earlier.
             .cache(myCache)
-            // Add an Interceptor to the OkHttpClient.
             .addInterceptor { chain ->
-
-                // Get the request from the chain.
                 var request = chain.request()
-
-                /*
-                *  Leveraging the advantage of using Kotlin,
-                *  we initialize the request and change its header depending on whether
-                *  the device is connected to Internet or not.
-                */
                 request = if (networkHelper.isNetworkConnected())
-                /*
-                *  If there is Internet, get the cache that was stored 5 seconds ago.
-                *  If the cache is older than 5 seconds, then discard it,
-                *  and indicate an error in fetching the response.
-                *  The 'max-age' attribute is responsible for this behavior.
-                */
                     request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
                 else
-                /*
-                *  If there is no Internet, get the cache that was stored 7 days ago.
-                *  If the cache is older than 7 days, then discard it,
-                *  and indicate an error in fetching the response.
-                *  The 'max-stale' attribute is responsible for this behavior.
-                *  The 'only-if-cached' attribute indicates to not retrieve new data; fetch the cache only instead.
-                */
                     request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
-                // End of if-else statement
-
-                // Add the modified request to the chain.
                 chain.proceed(request)
             }
             .build()
--- app/src/main/java/ir/moeindeveloper/iceandfire/ui/MainActivity.kt
+++ app/src/main/java/ir/moeindeveloper/iceandfire/ui/MainActivity.kt
@@ -17,5 +17,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        vm.quote.observe(this, Observer {
+            Log.e("qoute",it.toString())
+        })
     }
 }
